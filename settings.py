9acbeb6dcf8bf3ccfd32d67802a73f57d0a42923
Benjamin-C <bencrall@gmail.com>
2024-06-12 10:29:09 -0700

    Swapped serial ports for more common ones

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-# The serial port that the controller is on
-sender = "/dev/ttyACM1"
+# The serial port that the controller software is on
+sender = "/dev/ttyACM0"
 # The serial port the dummy sensor is on
-receiver = "/dev/ttyACM0"
+receiver = "/dev/ttyACM1"
 # The bitrate to use on the CAN bus
 bitrate = 1000000
 
