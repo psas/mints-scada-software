655f1456d8d44e0514c300956d8ed4a1792dee19
Repingjiang <lyp141125@gmail.com>
2026-03-10 18:29:56 -0700

    add devices settings template

--- settings.py
+++ settings.py
@@ -25,3 +25,72 @@ devices = (
     # {'name': 'Solenoid 6',         'class': 'Solenoid',        'display': 'SolenoidRow',     'address': 0x76},
     # {'name': 'Solenoid 7',         'class': 'Solenoid',        'display': 'SolenoidRow',     'address': 0x77},
 )
+
+
+# # The serial port that the controller software is on
+# sender = "/dev/ttyACM0"
+# # The serial port the dummy sensor is on
+# receiver = "/dev/ttyACM1"
+# # The bitrate to use on the CAN bus
+# bitrate = 1000000
+
+# # The sensors and actuators
+# devices = (
+#     # =========================================================
+#     # Devices with signal
+#     # =========================================================
+
+#     # Weight cells
+#     {'id': 'LC1', 'name': 'Weight Cell 1', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'LC2', 'name': 'Weight Cell 2', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'LC3', 'name': 'Weight Cell 3', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'LC4', 'name': 'Weight Cell 4', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+
+#     # Pressure transmitters
+#     {'id': 'PT41', 'name': 'Pressure Transmitter 41', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'PT42', 'name': 'Pressure Transmitter 42', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'PT43', 'name': 'Pressure Transmitter 43', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+#     {'id': 'PT44', 'name': 'Pressure Transmitter 44', 'class': 'GenericSensor', 'display': 'SensorRow', 'address': 0x000},
+
+#     # Temperature transmitters (placeholder; not in SVG yet)
+#     {'id': 'TT1', 'name': 'Temperature Transmitter 1', 'class': 'Thermocouple', 'display': 'ThermocoupleRow', 'address': 0x000},
+#     {'id': 'TT2', 'name': 'Temperature Transmitter 2', 'class': 'Thermocouple', 'display': 'ThermocoupleRow', 'address': 0x000},
+
+#     # Solenoid valves
+#     {'id': 'IG-XV-24', 'name': 'IG Solenoid Valve 24', 'class': 'Solenoid', 'display': 'SolenoidRow', 'address': 0x000},
+#     {'id': 'IG-XV-27', 'name': 'IG Solenoid Valve 27', 'class': 'Solenoid', 'display': 'SolenoidRow', 'address': 0x000},
+#     {'id': 'IPA-XV-23', 'name': 'IPA Solenoid Valve 23', 'class': 'Solenoid', 'display': 'SolenoidRow', 'address': 0x000},
+#     {'id': 'IPA-XV-25', 'name': 'IPA Solenoid Valve 25', 'class': 'Solenoid', 'display': 'SolenoidRow', 'address': 0x000},
+#     {'id': 'LOX-XV-26', 'name': 'LOX Solenoid Valve 26', 'class': 'Solenoid', 'display': 'SolenoidRow', 'address': 0x000},
+
+
+
+
+#     # =========================================================
+#     # Mechanical only (no signal, purely placeholder)
+#     # =========================================================
+
+#     # Physical watches
+#     {'id': 'IG-PC-21', 'name': 'IG Physical Watch 21', 'class': 'GenericSensor', 'display': 'None', 'address': 0x000},
+#     {'id': 'IG-PC-22', 'name': 'IG Physical Watch 22', 'class': 'GenericSensor', 'display': 'None', 'address': 0x000},
+
+#     # Check valves
+#     {'id': 'CV-011', 'name': 'Check Valve 011', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'CV-012', 'name': 'Check Valve 012', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'CV-013', 'name': 'Check Valve 013', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'CV-014', 'name': 'Check Valve 014', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+
+#     # Globe valves
+#     {'id': 'HV-036', 'name': 'Globe Valve 036', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'HV-039', 'name': 'Globe Valve 039', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'HV-040', 'name': 'Globe Valve 040', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'HV-051', 'name': 'Globe Valve 051', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+
+#     # Pressure safety valves
+#     {'id': 'IG-PSV-31', 'name': 'IG Pressure Safety Valve 31', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'IG-PSV-32', 'name': 'IG Pressure Safety Valve 32', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+#     {'id': 'IG-PSV-42', 'name': 'IG Pressure Safety Valve 42', 'class': 'GenericActuator', 'display': 'None', 'address': 0x000},
+
+
+
+# )
