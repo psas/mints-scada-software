916f18364e012bd7cabcdbe1aafd2023c6ae6a65
William Jeffreys <wjeffreys96@gmail.com>
2026-07-31 14:35:31 -0700

    create tasks module
    the api module is getting bloated so make a tasks module to separate
    concerns

--- src/mints_backend/api.py
+++ src/mints_backend/api.py
@@ -1,10 +1,11 @@
 import sys
 from logging import getLogger
-from queue import Full, Queue, ShutDown
 
 import can
 from box import Box
-from PySide6.QtCore import QObject, QRunnable, QThreadPool, Signal, Slot
+from PySide6.QtCore import QObject, QThreadPool, Signal
+
+from mints_backend.tasks import CANReceiveTask, CANSendTask
 
 log = getLogger(__name__)
 
@@ -85,76 +86,6 @@ class BackendApi(QObject):
         self.__stop()
         self.bus.shutdown()
         self._pool.waitForDone(250)
-
-
-class CANReceiveSignals(QObject):
-    sig_rx_message = Signal(can.Message)
-    sig_rx_error = Signal(str)
-
-
-class CANReceiveTask(QRunnable):
-    def __init__(self, bus: can.BusABC):
-        super().__init__()
-        self._bus = bus
-        self.signals = CANReceiveSignals()
-        self._running = True
-
-    @Slot()
-    def run(self):
-        while self._running:
-            try:
-                msg: can.Message | None = self._bus.recv()
-                if msg is None:
-                    log.debug("Unexpected null msg waiting for CAN rx")
-                    continue
-
-                self.signals.sig_rx_message.emit(msg)
-
-            except can.CanError as e:
-                log.error("CAN receive error: %s", e)
-                self.signals.sig_rx_error.emit(str(e))
-                continue
-
-    def stop(self):
-        self._running = False
-
-
-class CANSendSignals(QObject):
-    sig_tx_error = Signal(str)
-
-
-class CANSendTask(QRunnable):
-    def __init__(self, bus: can.BusABC):
-        super().__init__()
-        self._bus = bus
-        self.signals = CANSendSignals()
-        self._queue: Queue[can.Message] = Queue(50)
-        self._running = True
-
-    @Slot()
-    def run(self):
-        while self._running:
-            try:
-                msg = self._queue.get()
-                self._bus.send(msg)
-
-            except ShutDown:
-                break
-
-            except can.CanError as e:
-                log.error("CAN send error: %s", e)
-                self.signals.sig_tx_error.emit(str(e))
-
-    def on_msg_tx(self, msg: can.Message):
-        log.debug("Signal to send CAN msg received -- queuing message")
-        try:
-            self._queue.put_nowait(msg)
-        except Full as e:
-            self.signals.sig_tx_error.emit(f"Queue full -- dropping CAN msg -- {e}")
-
-    def stop(self):
-        self._running = False
-        self._queue.shutdown(immediate=True)
 
 
 class CANData:
