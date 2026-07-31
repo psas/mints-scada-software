edd0c93c65b3c21d579891658a2518564fce770c
William Jeffreys <wjeffreys96@gmail.com>
2026-07-30 23:48:09 -0700

    create datapacket class

--- src/mints_backend/api.py
+++ src/mints_backend/api.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+import sys
 from box import Box
 
 import can
@@ -7,6 +8,59 @@ from PySide6.QtCore import QObject, QRunnable, QThreadPool, Signal, Slot
 log = getLogger(__name__)
 
 SETTINGS = Box.from_toml(filename="settings.toml")
+
+NODE_ID_MASK = 0x7F
+
+CLAIM_NODE_MSG_ID = 0x180
+REQUEST_MSG_ID = 0x200
+RESPONSE_MSG_ID = 0x280
+
+SEQ_BYTE = 0
+CMD_BYTE = 1
+DATA_BYTE = 2
+
+
+class BackendApi(QObject):
+    sigMessage = Signal(object)
+    sigError = Signal(str)
+
+    def __init__(self, channel: str):
+        super().__init__()
+        log.debug("Initializing backend API")
+        try:
+            self.bus = can.interface.Bus(
+                interface=SETTINGS.can.interface,
+                channel=SETTINGS.can.channel if channel is None else channel,
+                bitrate=SETTINGS.can.bitrate,
+            )
+        except OSError as e:
+            log.error("Unable to connect to CAN bus -- %s", e.strerror)
+            sys.exit(e.errno)
+
+        self._pool = QThreadPool.globalInstance()
+        self._recv_task: CANReceiveTask | None = None
+
+    def start(self):
+        """Begin receiving CAN messages on a pooled thread."""
+        if self._recv_task is not None:
+            return  # already running
+        self._recv_task = CANReceiveTask(self.bus)
+        self._recv_task.signals.sigMessage.connect(self.sigMessage.emit)
+        self._recv_task.signals.sigError.connect(self.sigError.emit)
+        self._pool.start(self._recv_task)
+
+    def stop(self):
+        if self._recv_task is not None:
+            self._recv_task.stop()
+            self._recv_task = None
+
+    def send(self, msg: can.Message):
+        self.bus.send(msg)
+
+    def shutdown(self):
+        self.stop()
+        self._pool.waitForDone(2000)
+        self.bus.shutdown()
 
 
 class CANReceiveSignals(QObject):
@@ -40,39 +94,16 @@ class CANReceiveTask(QRunnable):
         self._running = False
 
 
-class BackendApi(QObject):
-    sigMessage = Signal(object)
-    sigError = Signal(str)
+class CanData:
+    def __init__(self, seq: int, cmd: int, bytes: bytearray):
+        self.seq = seq
+        self.cmd = cmd
+        self.bytes = bytes
 
-    def __init__(self, channel: str):
-        super().__init__()
-        log.debug("Initializing backend API")
-        self.bus = can.interface.Bus(
-            interface=SETTINGS.can.interface,
-            channel=SETTINGS.can.channel if channel is None else channel,
-            bitrate=SETTINGS.can.bitrate,
-        )
-        self._pool = QThreadPool.globalInstance()
-        self._recv_task: CANReceiveTask | None = None
 
-    def start(self):
-        """Begin receiving CAN messages on a pooled thread."""
-        if self._recv_task is not None:
-            return  # already running
-        self._recv_task = CANReceiveTask(self.bus)
-        self._recv_task.signals.sigMessage.connect(self.sigMessage.emit)
-        self._recv_task.signals.sigError.connect(self.sigError.emit)
-        self._pool.start(self._recv_task)
-
-    def stop(self):
-        if self._recv_task is not None:
-            self._recv_task.stop()
-            self._recv_task = None
-
-    def send(self, msg: can.Message):
-        self.bus.send(msg)
-
-    def shutdown(self):
-        self.stop()
-        self._pool.waitForDone(2000)
-        self.bus.shutdown()
+class DataPacket:
+    def __init__(self, msg: can.Message, err: int):
+        data = msg.data
+        self.id = msg.arbitration_id & NODE_ID_MASK
+        self.err = err
+        self.data = CanData(data[SEQ_BYTE], data[CMD_BYTE], data[DATA_BYTE:])
