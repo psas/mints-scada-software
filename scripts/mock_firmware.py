012993f0bed7511e22c17f790d5052b2dd3c5dca
William Jeffreys <wjeffreys96@gmail.com>
2026-08-07 14:52:52 -0700

    update mock_firmware to send a resp to setting an output
    this lets us set/unset state in the gui based on the output and means
    that our gui representation of the state of the output is accurate

--- scripts/mock_firmware.py
+++ scripts/mock_firmware.py
@@ -1,8 +1,9 @@
 import random
+from time import monotonic
 
 import can
+
 from config import config as CFG
-from mints_backend.device_manager import DeviceManager, Output, Sensor
 from mints_backend.datapacket import (
     ADDR_MSK,
     BASE_ID_MSK,
@@ -14,9 +15,9 @@ from mints_backend.datapacket import (
     CANData,
     DataPacket,
 )
+from mints_backend.device_manager import DeviceManager, Output, Sensor
 
 device_manager = DeviceManager(CFG["can"]["channel"])
-# device_manager.notifier.add_listener(can.Printer())
 outputs = {}
 for dev in device_manager.device_registry.values():
     match dev:
@@ -25,6 +26,10 @@ for dev in device_manager.device_registry.values():
 
         case _:
             pass
+
+# We just want the devices, not the bus
+device_manager.notifier.stop()
+device_manager.bus.shutdown()
 
 
 def main():
@@ -40,19 +45,15 @@ def main():
                 if msg is None:
                     continue
 
-                print(msg)
                 req_addr = msg.arbitration_id & ADDR_MSK
                 req_device = device_manager.device_registry[req_addr]
 
-                handle_can_rx(msg, req_device)
+                handle_can_rx(msg, req_device, bus)
         except KeyboardInterrupt:
-            print("Cleaning up...")
-            device_manager.notifier.stop()
-            device_manager.bus.shutdown()
-            print("Goodbye")
+            print("\nGoodbye")
 
 
-def handle_can_rx(msg: can.Message, req_device: Sensor | Output):
+def handle_can_rx(msg: can.Message, req_device: Sensor | Output, bus: can.BusABC):
     base_id = msg.arbitration_id & BASE_ID_MSK
 
     if base_id != REQUEST_MSG_ID:
@@ -72,20 +73,18 @@ def handle_can_rx(msg: can.Message, req_device: Sensor | Output):
         case CANCmd.SetOutput:
             val = request_datapacket.data.bytes[OUTPUT_SET_POS]
             outputs[req_device.id] = val
-            print(f"set output {req_device.id} to {val}")
+            resp_val = bytearray(CAN_DATA_LEN)
+            resp_val[OUTPUT_SET_POS] = outputs[req_device.id]
+            print(f"{monotonic()}: set output {hex(req_device.id)} to {val}")
 
         case CANCmd.GetOutput:
-            req_output = outputs[req_device.id]
-            resp_byte = bytearray([int(req_output)])
-            print(resp_byte)
-            resp_val = bytearray(
-                (bytearray([0] * (CAN_DATA_LEN - len(resp_byte))) + resp_byte)
-            )
+            resp_val = bytearray(CAN_DATA_LEN)
+            resp_val[OUTPUT_SET_POS] = outputs[req_device.id]
 
     if resp_val is not None:
         resp_data = CANData(cmd=None, bytes=resp_val)
         resp_datapacket = DataPacket(id=resp_id, is_err=False, data=resp_data)
-        device_manager.bus.send(resp_datapacket.to_can_message())
+        bus.send(resp_datapacket.to_can_message())
 
 
 if __name__ == "__main__":
