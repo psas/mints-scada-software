43b707ee1dd185bbe9eadbced9b122a553d0773a
greyland-tm <greyland.miller@gmail.com>
2026-08-15 14:12:18 -0700

    Corrected tests
    - Pulled in the toml files properly so they could be read
    - Created test toml files for each validator to fix false positive tests

--- tests/mints_backend/test_validation.py
+++ tests/mints_backend/test_validation.py
@@ -1,19 +1,29 @@
 import pytest
+import tomllib
 from pathlib import Path
 from pydantic import ValidationError
 from mints_backend.models import AdcChannelCfgModel, BoardCfgListModel, OutputCfgModel
 
-test_cfg_path = Path(__file__).parent.parent / "config.invalid_id_cfg"
+test_cfg_path = Path(__file__).parent.parent / "config"
 
 class TestValidation:
     def test_board_id_cfg(self):
+        invalid_board_id = test_cfg_path / "invalid_board_id.toml"
+        with invalid_board_id.open(mode = "rb") as file:
+            data = tomllib.load(file)
         with pytest.raises(ValidationError):
-            BoardCfgListModel.model_validate(test_cfg_path)
+            BoardCfgListModel.model_validate(data)
 
     def test_adc_sub_id_cfg(self):
+        invalid_adc_sub_id = test_cfg_path / "invalid_adc_sub_id.toml"
+        with invalid_adc_sub_id.open(mode = "rb") as file:
+            data = tomllib.load(file)["board"][0]["adc"]["channels"][0]
         with pytest.raises(ValidationError):
-            AdcChannelCfgModel.model_validate(test_cfg_path)
+            AdcChannelCfgModel.model_validate(data)
 
     def test_outputs_sub_id_cfg(self):
+        invalid_output_sub_id = test_cfg_path / "invalid_output_sub_id.toml"
+        with invalid_output_sub_id.open(mode = "rb") as file:
+            data = tomllib.load(file)["board"][1]["outputs"][0]
         with pytest.raises(ValidationError):
-            OutputCfgModel.model_validate(test_cfg_path)
+            OutputCfgModel.model_validate(data)
