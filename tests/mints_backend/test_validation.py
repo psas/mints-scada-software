3fa30917827535ef0b3de98fdbf28a00efbcd772
William Jeffreys <wjeffreys96@gmail.com>
2026-08-15 14:36:07 -0700

    Clearer names, add comments
    when reading the tests off the terminal its nice to be able to glean
    information about what the test does based on the function's name.
    Comments mirror other test suites now.

--- tests/mints_backend/test_validation.py
+++ tests/mints_backend/test_validation.py
@@ -8,24 +8,34 @@ from mints_backend.models import AdcChannelCfgModel, BoardCfgListModel, OutputCf
 
 test_cfg_path = Path(__file__).parent.parent / "config"
 
+
 class TestValidation:
-    def test_board_id_cfg(self):
+    def test_invalid_board_id_raises_exc(self):
+        """
+        The model should raise an exception if an invalid board_id is in the config
+        """
         invalid_board_id = test_cfg_path / "invalid_board_id.toml"
-        with invalid_board_id.open(mode = "rb") as file:
+        with invalid_board_id.open(mode="rb") as file:
             data = tomllib.load(file)
         with pytest.raises(ValidationError):
             BoardCfgListModel.model_validate(data)
 
-    def test_adc_sub_id_cfg(self):
+    def test_invalid_adc_sub_id_raises_exc(self):
+        """
+        The model should raise an exception if an invalid sub_id is in the config
+        """
         invalid_adc_sub_id = test_cfg_path / "invalid_adc_sub_id.toml"
-        with invalid_adc_sub_id.open(mode = "rb") as file:
+        with invalid_adc_sub_id.open(mode="rb") as file:
             data = tomllib.load(file)["board"][0]["adc"]["channels"][0]
         with pytest.raises(ValidationError):
             AdcChannelCfgModel.model_validate(data)
 
-    def test_outputs_sub_id_cfg(self):
+    def test_invalid_output_sub_id_raises_exc(self):
+        """
+        The model should raise an exception if an invalid sub_id is in the config
+        """
         invalid_output_sub_id = test_cfg_path / "invalid_output_sub_id.toml"
-        with invalid_output_sub_id.open(mode = "rb") as file:
+        with invalid_output_sub_id.open(mode="rb") as file:
             data = tomllib.load(file)["board"][1]["outputs"][0]
         with pytest.raises(ValidationError):
             OutputCfgModel.model_validate(data)
