bced64191a5a9ed3fec383eac1ec081a43ced90e
William Jeffreys <wjeffreys96@gmail.com>
2026-08-18 13:07:05 -0700

    Refactor tests, test dir structure
    Flesh out test infrastructure. Refactor to follow Arrange, Act, Assert,
    Cleanup test model. Use function tests instead of class tests for better
    test isolation.

---
+++ tests/conftest.py
@@ -0,0 +1,57 @@
+import can
+import pytest
+
+from config import config as CFG
+from mints_backend.device_manager import DeviceManager
+from mints_backend.models import BoardCfgListModel
+
+
+@pytest.fixture()
+def test_bus(request):
+    marker = request.node.get_closest_marker("loopback")
+    loopback = False if marker is None else marker.args[0]
+    bus: can.BusABC = can.ThreadSafeBus(
+        interface="virtual",
+        channel="vcan0",
+        bitrate=CFG["can"]["bitrate"],
+        receive_own_messages=loopback,
+    )
+
+    yield bus
+
+    bus.stop_all_periodic_tasks()
+    bus.shutdown()
+
+
+@pytest.fixture()
+def board_configs():
+    yield {
+        "board": [
+            {"board_id": 0x0, "outputs": [{"sub_id": 0x1, "name": "test"}]},
+            {
+                "board_id": 0x1,
+                "adc": {
+                    "channels": [{"sub_id": 0x0, "name": "p0", "kind": "pressure"}]
+                },
+            },
+        ]
+    }
+
+
+@pytest.fixture()
+def board_configs_model(board_configs):
+    yield BoardCfgListModel.model_validate(board_configs)
+
+
+@pytest.fixture()
+def device_manager(board_configs: dict):
+    device_manager = DeviceManager(
+        channel="vcan0", virtual_bus=True, board_cfg_dict=board_configs
+    )
+    yield device_manager
+    device_manager.teardown()
+
+
+@pytest.fixture()
+def registry(device_manager):
+    yield device_manager.device_registry
--- tests/mints_backend/test_validation.py
+++ tests/mints_backend/test_validation.py
@@ -1,4 +1,3 @@
-
 import pytest
 from pydantic import ValidationError
 
@@ -6,33 +5,47 @@ from config import boards as BOARD
 from mints_backend.models import AdcChannelCfgModel, BoardCfgListModel, OutputCfgModel
 
 
-class TestValidation:
-    def test_board_cfg_init_successfully(self):
-        """
-        The main BoardCfgListModel should initialize successfully under normal conditions
-        """
-        _validated_model = BoardCfgListModel.model_validate(BOARD)
+@pytest.fixture()
+def invalid_board_id():
+    yield {"board": [{"board_id": 0xFFF}]}
 
-    def test_invalid_board_id_raises_exc(self):
-        """
-        The model should raise an exception if an invalid board_id is in the config
-        """
-        data = {"board": [{"board_id": 0xFFF}]}
-        with pytest.raises(ValidationError):
-            BoardCfgListModel.model_validate(data)
 
-    def test_invalid_adc_sub_id_raises_exc(self):
-        """
-        The model should raise an exception if an invalid sub_id is in the config
-        """
-        data = {"sub_id": 0xFFF, "name": "PT1", "kind": "pressure"}
-        with pytest.raises(ValidationError):
-            AdcChannelCfgModel.model_validate(data)
+@pytest.fixture()
+def invalid_adc_chan_sub_id():
+    yield {"sub_id": 0xFFF, "name": "PT1", "kind": "pressure"}
 
-    def test_invalid_output_sub_id_raises_exc(self):
-        """
-        The model should raise an exception if an invalid sub_id is in the config
-        """
-        data = {"sub_id": 0xFFF, "name": "PT1"}
-        with pytest.raises(ValidationError):
-            OutputCfgModel.model_validate(data)
+
+@pytest.fixture()
+def invalid_output_sub_id():
+    yield {"sub_id": 0xFFF, "name": "PT1"}
+
+
+def test_board_cfg_init_successfully():
+    """
+    The main BoardCfgListModel should initialize successfully under normal conditions
+    """
+    _validated_model = BoardCfgListModel.model_validate(BOARD)
+
+
+def test_invalid_board_id_raises_exc(invalid_board_id):
+    """
+    The model should raise an exception if an invalid board_id is in the config
+    """
+    with pytest.raises(ValidationError):
+        BoardCfgListModel.model_validate(invalid_board_id)
+
+
+def test_invalid_adc_chan_sub_id_raises_exc(invalid_adc_chan_sub_id):
+    """
+    The model should raise an exception if an invalid sub_id is in the config
+    """
+    with pytest.raises(ValidationError):
+        AdcChannelCfgModel.model_validate(invalid_adc_chan_sub_id)
+
+
+def test_invalid_output_sub_id_raises_exc(invalid_output_sub_id):
+    """
+    The model should raise an exception if an invalid sub_id is in the config
+    """
+    with pytest.raises(ValidationError):
+        OutputCfgModel.model_validate(invalid_output_sub_id)
