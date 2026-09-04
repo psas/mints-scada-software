d652428030a693dc9907759a0adf53e2b71b7ca4
William Jeffreys <wjeffreys96@gmail.com>
2026-09-04 00:18:47 -0700

    Clean up device tree
    some of the code in this module was redundant, like the functions being
    passed to the device subscriptions/slot_fns. Also reorganize the module
    to follow the "higher level of abastraction as you read downwards"
    pattern I like

--- src/mints_gui/ui/widgets/device_tree.py
+++ src/mints_gui/ui/widgets/device_tree.py
@@ -23,6 +23,55 @@ SENSOR_UNITS: dict[SensorKind, str] = {
 }
 
 
+class DeviceParameterTree(ParameterTree):
+    def __init__(self, device_manager: DeviceManager, parent=None):
+        super().__init__(parent)
+        self.device_manager = device_manager
+        self._sensor_params: dict[str, Parameter] = {}
+        self.root = Parameter.create(name="Devices", type="group", children=[])
+        self.setParameters(self.root, showTop=False)
+        self._build_tree()
+
+    def _get_or_create_board_group(self, board_id: int) -> Parameter:
+        name = f"Board {hex(board_id)}"
+        if name in self.root.names:
+            return self.root.child(name)
+        group = Parameter.create(name=name, type="group", children=[])
+        self.root.addChild(group)
+        return group
+
+    def _build_tree(self) -> None:
+        for dev in self.device_manager.device_registry.values():
+            board_id = dev.id >> 4
+            board_group = self._get_or_create_board_group(board_id)
+            match dev:
+                case Output():
+                    self._add_output_param(board_group, dev)
+
+                case Sensor():
+                    self._add_sensor_param(board_group, dev)
+
+    def _add_output_param(self, board_group: Parameter, dev: Output) -> None:
+        param: OutputParameter = Parameter.create(name=dev.name, type="output")  # pyright: ignore[reportAssignmentType]
+        dev.add_slot_fn(param.update_from_backend)
+        param.sigValueChanged.connect(lambda _param, val: dev.set_state(val))
+        board_group.addChild(param)
+
+    def _add_sensor_param(self, board_group: Parameter, dev: Sensor) -> None:
+        unit = SENSOR_UNITS.get(dev.kind, "")
+        param = Parameter.create(
+            name=dev.name,
+            type="float",
+            value=0.0,
+            suffix=unit,
+            readonly=True,
+            tip=f"{dev.kind.value} sensor (id=0b{dev.id:b})",
+        )
+        board_group.addChild(param)
+        self._sensor_params[dev.name] = param
+        dev.subscribe(param.setValue)
+
+
 class OutputButton(QPushButton):
     sigChanged = Signal(bool)
 
@@ -92,68 +141,3 @@ class OutputParameter(Parameter):
 
 
 registerParameterType("output", OutputParameter)
-
-
-class DeviceParameterTree(ParameterTree):
-    def __init__(self, device_manager: DeviceManager, parent=None):
-        super().__init__(parent)
-        self.device_manager = device_manager
-        self._sensor_params: dict[str, Parameter] = {}
-        self.root = Parameter.create(name="Devices", type="group", children=[])
-        self.setParameters(self.root, showTop=False)
-        font = self.font()
-        font.setPointSize(10)
-        self.setFont(font)
-        self._build_tree()
-
-    def _get_or_create_board_group(self, board_id: int) -> Parameter:
-        name = f"Board {hex(board_id)}"
-        if name in self.root.names:
-            return self.root.child(name)
-        group = Parameter.create(name=name, type="group", children=[])
-        self.root.addChild(group)
-        return group
-
-    def _build_tree(self) -> None:
-        for dev in self.device_manager.device_registry.values():
-            board_id = dev.id >> 4
-            board_group = self._get_or_create_board_group(board_id)
-            match dev:
-                case Output():
-                    self._add_output_param(board_group, dev)
-
-                case Sensor():
-                    self._add_sensor_param(board_group, dev)
-
-    def _add_output_param(self, board_group: Parameter, dev: Output) -> None:
-        param: OutputParameter = Parameter.create(name=dev.name, type="output")  # pyright: ignore[reportAssignmentType]
-
-        def on_param_changed(_param, val: bool, dev=dev) -> None:
-            dev.set_state(OutputState.High if val else OutputState.Low)
-
-        @Slot(bool)
-        def on_can_rx(val: bool) -> None:
-            param.update_from_backend(val)
-
-        dev.add_slot_fn(on_can_rx)
-        param.sigValueChanged.connect(on_param_changed)
-        board_group.addChild(param)
-
-    def _add_sensor_param(self, board_group: Parameter, dev: Sensor) -> None:
-        unit = SENSOR_UNITS.get(dev.kind, "")
-        param = Parameter.create(
-            name=dev.name,
-            type="float",
-            value=0.0,
-            suffix=unit,
-            readonly=True,
-            tip=f"{dev.kind.value} sensor (id=0b{dev.id:b})",
-        )
-        board_group.addChild(param)
-        self._sensor_params[dev.name] = param
-
-        @Slot(int)
-        def on_value(val: int) -> None:
-            param.setValue(val)
-
-        dev.subscribe(on_value)
