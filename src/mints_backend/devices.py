4e748aa8acf73e4afc040a21cf7fb8a1b6e20c42
William Jeffreys <wjeffreys96@gmail.com>
2026-09-04 00:20:50 -0700

    Keep track of signal connections to devices with sets
    Keeping track of the signal connections allows better control over
    disconnecting, ensuring we wont disconnect a non existant connection,
    which pyqt doesnt like. This also means we can add the ability to
    disconnect specific slot fns, which I think we'll need for scripting

--- src/mints_backend/devices.py
+++ src/mints_backend/devices.py
@@ -18,7 +18,7 @@ from mints_backend.datapacket import (
 )
 from mints_backend.models import OutputState, SensorKind
 
-log = getLogger(__name__)
+logger = getLogger(__name__)
 
 UPDATE_PERIOD = 1.0
 
@@ -61,28 +61,58 @@ class Sensor(Device):
     def __init__(self, id: int, name: str, kind: SensorKind, bus: can.BusABC):
         super().__init__(id, name, bus)
         self.kind = kind
-        self.subscription: can.CyclicSendTaskABC | None = None
-        self.num_sig_val_rcv_connections = 0
+        self.send_task: can.CyclicSendTaskABC | None = None
+        self.subscribers: set[Callable] = set()
 
-    def subscribe(self, slot_fn: Callable, send_period: float = UPDATE_PERIOD):
+    def begin_periodic_reads(self, send_period: float):
         data = CANData(CANCmd.ReadReg, bytearray([0] * CAN_DATA_LEN))
         id = self.id + REQUEST_MSG_ID
         datapacket = DataPacket(id=id, is_err=False, data=data)
-        self.subscription = self.bus.send_periodic(
+        self.send_task = self.bus.send_periodic(
             datapacket.to_can_message(), send_period
         )
-        self.sig_value_received.connect(slot_fn)
-        self.num_sig_val_rcv_connections += 1
 
-    def unsubscribe_all(self):
-        if self.subscription is None:
+    def subscribe(self, slot_fn: Callable, send_period: float = UPDATE_PERIOD):
+        if slot_fn in self.subscribers:
+            logger.error(
+                "Attempted to subscribed already subscribed slot fn to %s: %s",
+                self.name,
+                str(slot_fn),
+            )
             return
-        self.subscription.stop()
-        if self.num_sig_val_rcv_connections > 0:
-            self.sig_value_received.disconnect()
-            self.num_sig_val_rcv_connections = 0
-        else:
+
+        if len(self.subscribers) == 0:
+            self.begin_periodic_reads(send_period)
+
+        self.sig_value_received.connect(slot_fn)
+        self.subscribers.add(slot_fn)
+
+    def unsubscribe(self, slot_fn: Callable) -> None:
+        if slot_fn not in self.subscribers:
+            logger.error(
+                "Attempted to unsubscribe slot fn not subscribed to %s: %s",
+                self.name,
+                str(slot_fn),
+            )
             return
+
+        self.sig_value_received.disconnect(slot_fn)
+        self.subscribers.remove(slot_fn)
+        if len(self.subscribers) == 0:
+            self.stop_send_task()
+
+    def unsubscribe_all(self) -> None:
+        self.stop_send_task()
+        for slot_fn in self.subscribers:
+            self.sig_value_received.disconnect(slot_fn)
+        self.subscribers.clear()
+
+    def stop_send_task(self) -> None:
+        if self.send_task is None:
+            logger.error("Attempted to stop non-running send task for %s", self.name)
+            return
+
+        self.send_task.stop()
 
     @override
     def decode(self, datapacket: DataPacket) -> int:
@@ -96,12 +126,11 @@ class Output(Device):
     def __init__(self, id: int, name: str, bus: can.BusABC):
         super().__init__(id, name, bus)
         self.state: OutputState | None = None
-        self.num_sig_val_rcv_connections = 0
+        self.sig_val_recv_connections: set[Callable] = set()
 
-    def set_state(self, state: OutputState) -> None:
+    def set_state(self, val: bool) -> None:
         bytes = bytearray(CAN_DATA_LEN)
-        bytes[OUTPUT_SET_POS] = state.value
-
+        bytes[OUTPUT_SET_POS] = int(val)
         self.send_cmd(CANCmd.SetOutput, bytes)
 
     def get_state(self) -> None:
@@ -109,12 +138,22 @@ class Output(Device):
 
     def add_slot_fn(self, slot_fn: Callable) -> None:
         self.sig_value_received.connect(slot_fn)
-        self.num_sig_val_rcv_connections += 1
+        self.sig_val_recv_connections.add(slot_fn)
+
+    def remove_slot_fn(self, slot_fn: Callable):
+        if slot_fn not in self.sig_val_recv_connections:
+            logger.error(
+                "Attempted to remove slot fn not connected to %s: %s",
+                self.name,
+                str(slot_fn),
+            )
+        self.sig_value_received.disconnect(slot_fn)
+        self.sig_val_recv_connections.remove(slot_fn)
 
     def remove_all_slot_fns(self) -> None:
-        if self.num_sig_val_rcv_connections > 0:
-            self.sig_value_received.disconnect()
-            self.num_sig_val_rcv_connections = 0
+        for slot_fn in self.sig_val_recv_connections:
+            self.sig_value_received.disconnect(slot_fn)
+        self.sig_val_recv_connections.clear()
 
     def decode(self, datapacket: DataPacket) -> int:
         return datapacket.data.bytes[OUTPUT_SET_POS]
---
+++ tests/mints_backend/test_devices.py
@@ -0,0 +1,150 @@
+import threading
+
+import can
+import pytest
+from pytestqt.qtbot import QtBot
+
+from mints_backend.datapacket import CAN_DATA_LEN, RESPONSE_MSG_ID, CANData, DataPacket
+from mints_backend.device_manager import BoardCfgListModel, DeviceManager
+from mints_backend.devices import Device, Output, Sensor
+from mints_backend.models import SensorKind
+
+
+@pytest.fixture()
+def event_bool():
+    event = threading.Event()
+    yield event
+    event.clear()
+
+
+@pytest.fixture()
+def device(dev_bus: can.ThreadSafeBus):
+    yield Device(id=0x0, name="TEST", bus=dev_bus)
+
+
+@pytest.fixture()
+def sensor(dev_bus: can.ThreadSafeBus, notifier: can.Notifier):
+    sensor = Sensor(id=0x1, name="TEST0", kind=SensorKind.Temperature, bus=dev_bus)
+    notifier.add_listener(sensor.handle_can_rx)
+    yield sensor
+
+
+@pytest.fixture()
+def sensor_datapacket(sensor: Sensor):
+    resp_id = RESPONSE_MSG_ID | sensor.id
+    resp_data = CANData(None, bytearray(CAN_DATA_LEN))
+    yield DataPacket(id=resp_id, is_err=False, data=resp_data)
+
+
+@pytest.fixture()
+def output(dev_bus: can.ThreadSafeBus, notifier: can.Notifier):
+    output = Output(id=0x2, name="Test1", bus=dev_bus)
+    notifier.add_listener(output.handle_can_rx)
+    yield output
+
+
+@pytest.fixture()
+def output_datapacket(output: Output):
+    resp_id = RESPONSE_MSG_ID | output.id
+    resp_data = CANData(None, bytearray(CAN_DATA_LEN))
+    yield DataPacket(id=resp_id, is_err=False, data=resp_data)
+
+
+def test_sensor_init_success(sensor: Sensor):
+    """
+    Sensor devices should initialize successfully under normal conditions
+    """
+    assert sensor.id is not None
+    assert sensor.name is not None
+    assert isinstance(sensor.bus, can.ThreadSafeBus)
+
+
+def test_output_init_success(output: Output):
+    """
+    Output devices should initialize successfully under normal conditions
+    """
+    assert output.id is not None
+    assert output.name is not None
+    assert isinstance(output.bus, can.ThreadSafeBus)
+
+
+def test_sensor_subscribe_unsubscribe(sensor: Sensor, test_bus: can.ThreadSafeBus):
+    """
+    Sensors should send CAN messages when subscribed to, and shouldn't send messages when unsubscribed from
+    """
+
+    def test_fn(_args):
+        return
+
+    sensor.subscribe(test_fn, send_period=0.01)
+    assert test_bus.recv(timeout=0.01) is not None
+    sensor.unsubscribe(test_fn)
+    assert test_bus.recv(timeout=0.02) is None
+
+
+def test_sensor_unsubscribe_all(sensor: Sensor, test_bus: can.ThreadSafeBus):
+    """
+    unsubscribe_all should unsubscribe all slot functions from a sensor
+    """
+
+    def test_fn1(_args):
+        return
+
+    def test_fn2(_args):
+        return
+
+    sensor.subscribe(test_fn1, send_period=0.01)
+    sensor.subscribe(test_fn2, send_period=0.01)
+    assert test_bus.recv(timeout=0.01) is not None
+    sensor.unsubscribe_all()
+    assert test_bus.recv(timeout=0.02) is None
+
+
+def test_decode_on_nonsubclassed_device_raises_exc(
+    device: Device, output_datapacket: DataPacket
+):
+    """
+    Calling decode on a non-subclassed Device should raise an exception
+    """
+    with pytest.raises(NotImplementedError):
+        device.decode(output_datapacket)
+
+
+def test_sensor_rx_handler_called(
+    qtbot: QtBot,
+    sensor: Sensor,
+    sensor_datapacket: DataPacket,
+    test_bus: can.ThreadSafeBus,
+    event_bool: threading.Event,
+):
+    """
+    When a sensor receives a CAN msg addressed to it, it should fire its can_rx_handler
+    """
+    sensor.subscribe(lambda _arg: event_bool.set(), send_period=0.1)
+
+    with qtbot.waitSignal(
+        sensor.sig_value_received, raising=True, timeout=100
+    ) as _blocker:
+        test_bus.send(sensor_datapacket.to_can_message())
+
+    assert event_bool.is_set(), f"sensor {id} handler was not called"
+
+
+def test_output_rx_handler_called(
+    qtbot: QtBot,
+    output: Output,
+    output_datapacket: DataPacket,
+    test_bus: can.ThreadSafeBus,
+    event_bool: threading.Event,
+):
+    """
+    When an output device receives a CAN msg addressed to it, it should fire its can_rx_handler
+    """
+    output.add_slot_fn(lambda _arg: event_bool.set())
+
+    with qtbot.waitSignal(
+        output.sig_value_received, raising=True, timeout=1000
+    ) as _blocker:
+        test_bus.send(output_datapacket.to_can_message())
+
+    assert event_bool.is_set(), f"output {id} handler was not called"
